20f5279abf3d0f3d0fac6b391545bf74d1cb0ec9
adeeva786 <adeevaga@gmail.com>
2025-11-14 15:33:32 +0530

    hello

--- astricks.c
+++ astricks.c
@@ -12,6 +12,20 @@ void main(){
             printf("*");
         };
         printf("\n");
+        
+
+    return -1;
+}int main()
+{
+    int arr[] = { 25,50,75,100 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+
+    int key = 100;
+
+    // Function call
+    int position = findElement(arr, n, key);
+
     };
     for(int i=5;i>=1;i--){
         for(int j=i;j>=1;j--){
